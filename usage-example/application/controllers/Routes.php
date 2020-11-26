78530864369f6c12cdb64f5aca671e645b05f658
Andrew Koster <c.andrew.koster@gmail.com>
2020-11-25 21:46:18 -0700

    Added separate display for routing options.

--- usage-example/application/controllers/Routes.php
+++ usage-example/application/controllers/Routes.php
@@ -13,8 +13,13 @@ class Routes extends CI_Controller
     }
 
     echo "# Routes\n\n";
-    foreach (CodeIgniterRoutesFinder::find() as $route_url => $controller_method) {
-      echo $route_url . ': ' . (is_string($controller_method) ? $controller_method : json_encode($controller_method)) . "\n";
+    foreach (CodeIgniterRoutesFinder::all_routes() as $route_url => $controller_method) {
+      echo "- " . $route_url . ': ' . (is_string($controller_method) ? $controller_method : json_encode($controller_method)) . "\n";
     }
+
+    echo "\n## Routing options\n\n";
+    $routes_options = CodeIgniterRoutesFinder::routing_options();
+    echo "- default_controller_url: " . $routes_options->default_controller_url() . "\n";
+    echo "- translate_uri_dashes: " . ($routes_options->translate_uri_dashes() ? "Yes" : "No") . "\n";
   }
 }
